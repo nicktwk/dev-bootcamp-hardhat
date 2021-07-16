218dbeeb44b62b604229aabd4074f65f2554aa1d
Nick Thomas <nickthomas@Nicks-MacBook-Pro.local>
2021-07-16 12:43:58 -0700

    Developer Bootcamp.

--- hardhat.config.js
+++ hardhat.config.js
@@ -15,6 +15,7 @@ require("./tasks/block-number")
 require("./tasks/random-number-consumer")
 require("./tasks/price-consumer")
 require("./tasks/api-consumer")
+require('solidity-coverage')
 
 
 require('dotenv').config()
@@ -24,28 +25,31 @@ const RINKEBY_RPC_URL = process.env.RINKEBY_RPC_URL || "https://eth-rinkeby.alch
 const KOVAN_RPC_URL = process.env.KOVAN_RPC_URL || "https://eth-kovan.alchemyapi.io/v2/your-api-key"
 const MNEMONIC = process.env.MNEMONIC || "your mnemonic"
 const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY || "Your etherscan API key"
-// optional
+
 const PRIVATE_KEY = process.env.PRIVATE_KEY || "your private key"
 
 module.exports = {
-    defaultNetwork: "hardhat",
+    defaultNetwork: "kovan",
     networks: {
         hardhat: {
-            // // If you want to do some forking, uncomment this
-            // forking: {
-            //   url: MAINNET_RPC_URL
+            // If you want to do some forking, uncomment this
+            //forking: {
+            // url: MAINNET_RPC_URL
             // }
+
         },
+
         localhost: {
         },
         kovan: {
             url: KOVAN_RPC_URL,
-            // accounts: [PRIVATE_KEY],
-            accounts: {
-                mnemonic: MNEMONIC,
-            },
+            accounts: [PRIVATE_KEY],
+            //accounts: {
+            //    mnemonic: MNEMONIC,
+            //},
             saveDeployments: true,
         },
+
         rinkeby: {
             url: RINKEBY_RPC_URL,
             // accounts: [PRIVATE_KEY],
@@ -53,6 +57,7 @@ module.exports = {
                 mnemonic: MNEMONIC,
             },
             saveDeployments: true,
+
         },
         ganache: {
             url: 'http://localhost:8545',
